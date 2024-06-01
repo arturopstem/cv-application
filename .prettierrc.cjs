33c3e59b0514bb152935beb9dc21caaeb5301908
arturop <67555134+arturopstem@users.noreply.github.com>
2024-05-31 23:58:56 -0500

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  htmlWhitespaceSensitivity: 'ignore',
+  singleQuote: true,
+  overrides: [{ files: ['*.css'], options: { singleQuote: false } }],
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
+
+module.exports = config;
