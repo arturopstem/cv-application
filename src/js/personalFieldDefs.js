81b2e9411dcdad0f3b2bb590b4f96e9b90294960
arturop <67555134+arturopstem@users.noreply.github.com>
2024-10-17 00:34:32 -0500

    refactor(personalFieldDefs): Rename variable to personalFieldDefinitions

--- src/js/personalFieldDefs.js
+++ src/js/personalFieldDefs.js
@@ -1,4 +1,4 @@
-const fieldDefinitions = {
+const personalFieldDefinitions = {
   firstName: {
     fieldType: 'input',
     type: 'text',
@@ -54,4 +54,4 @@ const fieldDefinitions = {
   },
 };
 
-export default fieldDefinitions;
+export default personalFieldDefinitions;
