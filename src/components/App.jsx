88547a0babb460886c1a861621329d87a7a30de4
arturop <67555134+arturopstem@users.noreply.github.com>
2024-10-29 22:54:59 -0500

    feat: Implement App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import { useImmer } from 'use-immer';
+
+import { getInitialProfile } from '../js/profileStorage';
+import '../styles/App.css';
+import Header from './Header';
+import Main from './Main';
+import Navigation from './Navigation';
+
 function App() {
-  return <></>;
+  const [profile, setProfile] = useImmer(getInitialProfile);
+  const [currentView, setCurrentView] = useState('personal-details');
+
+  return (
+    <>
+      <Header />
+      <Navigation {...{ currentView, setCurrentView }} />
+      <Main {...{ currentView, profile, setProfile }} />
+    </>
+  );
 }
 
 export default App;
