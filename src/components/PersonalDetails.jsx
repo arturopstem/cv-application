97019f65c169ff51d75aa8a4f9fd40c5256cfaaf
arturop <67555134+arturopstem@users.noreply.github.com>
2024-10-29 22:40:49 -0500

    feat: Add save functionality to PersonalDetails

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,12 +1,19 @@
+import { savePersonalDetails } from '../js/profileStorage';
 import PersonalField from './PersonalField';
 
 function PersonalDetails({ profile, setProfile }) {
   const hook = { profile, setProfile };
+  const { personalDetails } = profile;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    savePersonalDetails(personalDetails);
+  };
 
   return (
     <section className="personal-details">
       <h2>Personal Details</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <PersonalField field="firstName" {...hook} />
         <PersonalField field="lastName" {...hook} />
         <PersonalField field="emailAddress" {...hook} />
@@ -14,6 +21,13 @@ function PersonalDetails({ profile, setProfile }) {
         <PersonalField field="dateOfBirth" {...hook} />
         <PersonalField field="gender" {...hook} />
         <PersonalField field="nationality" {...hook} />
+        <menu>
+          <li>
+            <button className="primary-button" type="submit">
+              Save
+            </button>
+          </li>
+        </menu>
       </form>
     </section>
   );
