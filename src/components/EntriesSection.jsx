18900d31f849ab31fe88257619a560aab6139cd5
arturop <67555134+arturopstem@users.noreply.github.com>
2024-10-31 02:30:44 -0500

    feat: Add animations to entries section using @formkit/auto-animate

--- src/components/EntriesSection.jsx
+++ src/components/EntriesSection.jsx
@@ -1,3 +1,4 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { v4 as uuidv4 } from 'uuid';
 
 import sectionDefinitions from '../js/sectionDefinitions';
@@ -5,6 +6,7 @@ import AddEntryButton from './AddEntryButton';
 import EntryForm from './EntryForm';
 
 function EntriesSection({ section, profile, setProfile }) {
+  const [parent] = useAutoAnimate({ disrespectUserMotionPreference: true });
   const entries = profile[section];
 
   const def = sectionDefinitions.find((def) => def.name === section);
@@ -18,7 +20,7 @@ function EntriesSection({ section, profile, setProfile }) {
   };
 
   return (
-    <section className="entries-section">
+    <section ref={parent} className="entries-section">
       <h2>{def.title}</h2>
       {entries.map((entry) => (
         <EntryForm key={entry.id} {...{ section, entry, setProfile }} />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+
 import EntriesSection from './EntriesSection';
 import Template from './Template';
 
 function Main({ currentView: section, profile, setProfile }) {
+  const [parent] = useAutoAnimate({ disrespectUserMotionPreference: true });
   let mainContent = <>Loading ...</>;
 
   const entriesSectionProps = { section, profile, setProfile };
@@ -11,14 +14,14 @@ function Main({ currentView: section, profile, setProfile }) {
     case 'work':
     case 'education':
     case 'skill':
-      mainContent = <EntriesSection {...entriesSectionProps} />;
+      mainContent = <EntriesSection key={section} {...entriesSectionProps} />;
       break;
     case 'template':
       mainContent = <Template />;
       break;
   }
 
-  return <main>{mainContent}</main>;
+  return <main ref={parent}>{mainContent}</main>;
 }
 
 export default Main;
